57711112e0885fad72a68f378b11713d5bc87de1
Gustavo Guzman <guzmangustavo@outlook.com>
2025-04-18 18:19:35 +0000

    Add docstrings for database-related functions

--- api_service/core/db.py
+++ api_service/core/db.py
@@ -18,9 +18,28 @@ engine = create_engine(DATABASE_URL, echo=True)
 
 
 def create_db_and_tables():
+    """
+    Creates the database and all tables defined in the SQLModel metadata.
+
+    This function uses the SQLModel engine to connect to the database and 
+    create all the tables that are mapped to SQLModel models.
+
+    Returns:
+        None
+    """
     SQLModel.metadata.create_all(engine)
 
 
 def get_session():
+    """
+    Provides a session for database operations.
+
+    This function creates a new database session using the SQLModel engine. 
+    The session is used to interact with the database and is automatically 
+    closed when the operations are complete.
+
+    Yields:
+        Session: A SQLModel session for interacting with the database.
+    """
     with Session(engine) as session:
         yield session
